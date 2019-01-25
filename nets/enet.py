596ecb218bb8476b148d8e3d574783194eaf21c1
Alf-Rune Siqveland <alf.rune@siqveland.no>
2019-01-25 19:07:35 +0100

    Added blank network proposals

---
+++ nets/enet.py
@@ -0,0 +1,5 @@
+class ENet:
+    """
+    https://arxiv.org/pdf/1606.02147.pdf
+    """
+    def __init__(self):
---
+++ nets/icnet.py
@@ -0,0 +1,7 @@
+class ICNet:
+    """
+    http://openaccess.thecvf.com/content_ECCV_2018/papers/Hengshuang_Zhao_ICNet_for_Real-Time_ECCV_2018_paper.pdf
+    """
+    def __init__(self):
+        """
+        """
