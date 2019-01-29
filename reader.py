ebab9bf85eb4fc8f884d854e6a18855619297a91
Alf-Rune Siqveland <alf.rune@siqveland.no>
2019-01-29 17:29:25 +0100

    Started on implementing the script for generating tfrecords

---
+++ reader.py
@@ -0,0 +1,16 @@
+import json
+
+import tensorflow as tf
+
+class DatasetReader:
+    """
+    Should contain the input stage for reading, preprocessing and possibly
+    augmenting (maybe as a closure) for an arbitrary.
+    Not sure if input transformations should be applied that involves
+    rescaling.
+    """
+
+    def __init__(self, path):
+
+    def __iter__(self):
+
