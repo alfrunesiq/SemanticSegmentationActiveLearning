c6c94a8adbf16734f580394f1550fe9e0ce936fe
Alf-Rune Siqveland <alf.rune@siqveland.no>
2019-02-21 18:34:44 +0100

    Added argument in freiburg support layer for validation set
    Signed-off-by: Alf-Rune Siqveland <alf.rune@siqveland.no>

--- dataset/support/freiburg.py
+++ dataset/support/freiburg.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function, absolute_import, division
 from collections import namedtuple
 
@@ -43,6 +42,14 @@ class Freiburg:
         self.modalities = modalities
 
     def label_mapping(self, label_image):
+        """
+        Maps the rgb label image to the respective ids given in dataset
+        README file.
+        :param label_image: tf.Tensor with the label image
+        :returns: The remapped label image
+        :rtype:   tf.Tensor (single channel image)
+
+        """
         # TODO insert docstring here
         _, green, _ = tf.split(label_image, 3, axis=-1)
         green_int32 = tf.to_int32(green)
@@ -51,11 +58,20 @@ class Freiburg:
     def get_label_mapping(self):
         return green2trainId
 
-    def file_associations(self, root_path):
-        #TODO add docstring
+    def file_associations(self, root_path, val_proportion=0.05):
+        """
+        Creates a dictionary of file associations, due to the awkward
+        naming convension used for this dataset the code below looks a
+        bit messy.
+        :param root_path:      dataset root path
+        :param val_proportion: proportion of training images moved to
+                               validation set
+        :returns: dictionary with fileassociations
+        :rtype:   dict {"split": {"id": {"type": "path"}}}
+                  type in e.g. "image" / "label" / "nir"
+        """
         image_dir = "rgb"
         label_dir = "GT_color"
-        nir_dir   = "nir_gray"
         if self.modalities == None:
             self.modalities = ["rgb"]
         elif not isinstance(self.modalities, list):
@@ -63,11 +79,11 @@ class Freiburg:
                 "ERROR: Modalities need to be a list of strings "
                 "containing the name of modalities as in the dataset "
                 "filetree, e.g. {rgb,nir_gray,...}.")
-
         _file_associations = {
             "train": {},
-            "test":  {}
+            "test" : {}
         }
+
         # _file_associations = {split: {id: {image/label/nir: path}}}
         for split in _file_associations.keys():
             # setup paths
@@ -89,4 +105,20 @@ class Freiburg:
                     _id = filename.split(".")[0].split("_")[0]
                     file_path = os.path.join(mod_path, filename)
                     _file_associations[split][_id][modality] = file_path
+
+        # Move some training examples to validation set
+        _file_associations["val"] = {}
+        # Calculate stride used for uniformly sampling the training ids
+        stride = len(_file_associations["train"])
+        if val_proportion > 0.0:
+            stride = int((1.0 / val_proportion + 1))
+
+        # Sample uniformly from the sorted list of training ids
+        examples = [example for example in sorted(
+                            _file_associations["train"])[::stride]]
+        # Move samples from train dict to val dict
+        for example in examples:
+            _file_associations["val"][example] = \
+                _file_associations["train"].pop(example)
+
         return _file_associations
