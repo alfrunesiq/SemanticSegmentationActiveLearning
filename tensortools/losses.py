Fixed loginverse scaling to rescale to a minimum of 1.0
Signed-off-by: Alf-Rune Siqveland <alf.rune@siqveland.no>

tensortools/losses.py
@@ -24,6 +24,7 @@ def masked_softmax_cross_entropy(
     :returns: The mean cross entropy loss
     :rtype:   tf.Tensor: @logits.dtype (tf.float32)
     """
+    _EULER = 1.718281828459045 # Euler's constant
     with tf.name_scope(scope):
         # Squeeze out that singleton dimension
         _labels = labels
@@ -46,7 +47,10 @@ def masked_softmax_cross_entropy(
             _mask = tf.cast(mask, dtype=tf.float32)
         if weight > 1.0: # ENet type mask weighting
             p_class = tf.reduce_sum(tf.nn.softmax(logits) * _labels_oh, axis=-1)
-            w_class = tf.math.divide(1.0, tf.math.log(weight + p_class))
+            # NOTE: second term in denominator below ensures lower bound is 1
+            w_class = tf.math.divide(
+                    1.0, tf.math.log(weight + (_EULER-weight)*p_class)
+            )
             _mask = _mask * w_class
 
         loss = tf.nn.softmax_cross_entropy_with_logits_v2(